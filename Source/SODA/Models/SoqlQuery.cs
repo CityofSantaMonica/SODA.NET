51fa297be93ce674c3ff65e4c9404cf18734a343
Kegan Maher <Kegan.Maher@SMGOV.NET>
2014-07-16 18:16:27 +0000

    TDD for SoqlQuery

--- Source/SODA/Models/SoqlQuery.cs
+++ Source/SODA/Models/SoqlQuery.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -12,20 +13,25 @@ namespace SODA.Models
 
     public class SoqlQuery
     {
-        private static string delimiter = ",";
-
-        private string[] select { get; set; }
+        public static readonly string Delimiter = ",";
+        public static readonly string SelectKey = "$select";
+        public static readonly string WhereKey = "$where";
+        public static readonly string OrderKey = "$order";
+        public static readonly string GroupKey = "$group";
+        public static readonly string LimitKey = "$limit";
+        public static readonly string OffsetKey = "$offset";
+        
+        private IEnumerable<string> select { get; set; }
         private string where { get; set; }
-        private string[] groupBy { get; set; }
-        private string having { get; set; }
         private SortOrder sortOrder { get; set; }
-        private string[] orderBy { get; set; }
-        private uint limit { get; set; }
-        private uint offset { get; set; }        
+        private IEnumerable<string> orderBy { get; set; }
+        private IEnumerable<string> groupBy { get; set; }
+        private int limit { get; set; }
+        private int offset { get; set; }                
 
         public SoqlQuery Select(params string[] columns)
         {
-            select = columns;
+            select = getNonEmptyValues(columns);
             return this;
         }
 
@@ -35,68 +41,82 @@ namespace SODA.Models
             return this;
         }
 
-        public SoqlQuery GroupBy(params string[] columns)
+        public SoqlQuery Order(params string[] columns)
         {
-            groupBy = columns;
+            return Order(SortOrder.ASC, columns);
+        }
+
+        public SoqlQuery Order(SortOrder sortOrder, params string[] columns)
+        {
+            this.sortOrder = sortOrder;
+            orderBy = getNonEmptyValues(columns);
             return this;
         }
 
-        public SoqlQuery Having(string predicate)
+        public SoqlQuery Group(params string[] columns)
         {
-            having = predicate;
+            groupBy = getNonEmptyValues(columns);
             return this;
         }
-
-        public SoqlQuery OrderBy(SortOrder order = SortOrder.ASC, params string[] columns)
+        
+        public SoqlQuery Limit(int limit)
         {
-            sortOrder = order;
-            orderBy = columns;
-            return this;
-        }
+            //limit < 0 makes no sense, take the absolute value
+            limit = Math.Abs(limit);
 
-        public SoqlQuery Limit(uint limit)
-        {
             //limit > 1000 will return a 400 Bad Request
             //http://dev.socrata.com/docs/queries.html#the_limit_parameter
+            limit = limit > 1000 ? 1000 : limit;
 
-            this.limit = Math.Max(limit, 1000);
+            this.limit = limit;
             return this;
         }
 
-        public SoqlQuery Offset(uint offset)
+        public SoqlQuery Offset(int offset)
         {
+            //offset < 0 makes no sense, take the absolute value
+            offset = Math.Abs(offset);
+
             this.offset = offset;
+
             return this;
         }
         
         public override string ToString()
         {
-            var sb = new StringBuilder("$select=");
+            var sb = new StringBuilder(SelectKey + "=");
 
             if(select != null && select.Any())
-                sb.Append(String.Join(delimiter, select));
+                sb.Append(String.Join(Delimiter, select));
             else
                 sb.Append("*");
 
             if (!String.IsNullOrEmpty(where))
-                sb.AppendFormat("&$where={0}", where);
+                sb.AppendFormat("&{0}={1}", WhereKey, where);
 
             if (groupBy != null && groupBy.Any())
-                sb.AppendFormat("&$group={0}", String.Join(delimiter, groupBy));
-
-            if (!String.IsNullOrEmpty(having))
-                sb.AppendFormat("&$having={0}", having);
+                sb.AppendFormat("&{0}={1}", GroupKey, String.Join(Delimiter, groupBy));
 
             if (orderBy != null && orderBy.Any())
-                sb.AppendFormat("&$order={0} {1}", String.Join(delimiter, orderBy), sortOrder);
+                sb.AppendFormat("&{0}={1} {2}", OrderKey, String.Join(Delimiter, orderBy), sortOrder);
 
             if (offset > 0)
-                sb.AppendFormat("&$offset={0}", offset);
+                sb.AppendFormat("&{0}={1}", OffsetKey, offset);
 
             if (limit > 0)
-                sb.AppendFormat("&$limit={0}", limit);
+                sb.AppendFormat("&{0}={1}", LimitKey, limit);
 
             return sb.ToString();
-        } 
+        }
+
+        private static IEnumerable<string> getNonEmptyValues(IEnumerable<string> source)
+        {
+            if (source != null && source.Any())
+            {
+                return source.Where(s => !String.IsNullOrEmpty(s));
+            }
+
+            return null;
+        }
     }
 }
---
+++ Source/Test/Unit/SoqlTests.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using SODA.Models;
+
+namespace SODA.Tests.Unit
+{
+    [TestFixture]
+    public class SoqlTests
+    {
+        private static string selectStar = String.Format("{0}={1}", SoqlQuery.SelectKey, "*");
+        
+        [Test]
+        public void New_Soql_Selects_Star()
+        {
+            string soql = new SoqlQuery().ToString();
+            
+            Assert.AreEqual(selectStar, soql);
+        }
+
+        [Test]
+        public void Empty_Select_Selects_Star()
+        {
+            string emptySelect = new SoqlQuery().Select("").ToString();
+            string manyEmptySelect = new SoqlQuery().Select("", "", "").ToString();
+            string nullSelect = new SoqlQuery().Select(null).ToString();
+
+            Assert.AreEqual(selectStar, emptySelect);
+            Assert.AreEqual(selectStar, nullSelect);
+        }
+
+        [TestCase("column1", "")]
+        [TestCase("column1", "", "column2")]
+        public void Select_Clause_Only_Gets_Valid_Columns(params string[] columns)
+        {
+            string expected = String.Format("{0}={1}", SoqlQuery.SelectKey, String.Join(SoqlQuery.Delimiter, columns.Where(c => !String.IsNullOrEmpty(c))));
+
+            string soql = new SoqlQuery().Select(columns).ToString();
+
+            Assert.AreEqual(expected, soql);
+        }
+        
+        [Test]
+        public void Last_Select_Overwrites_All_Previous()
+        {
+            string[] first =  { "first" };
+            string[] second = { "first", "second" };
+            string[] last =  { "first", "second", "last" };
+            
+            string expected = String.Format("{0}={1}", SoqlQuery.SelectKey, String.Join(SoqlQuery.Delimiter, last));
+            
+            SoqlQuery soqlQuery = new SoqlQuery();
+
+            soqlQuery.Select(first)
+                     .Select(second)
+                     .Select(last);
+
+            Assert.AreEqual(expected, soqlQuery.ToString());
+        }
+
+        [Test]
+        public void Empty_Where_Ignores_Where_Clause()
+        {
+            string emptyWhere = new SoqlQuery().Where("").ToString();
+            string nullWhere = new SoqlQuery().Where(null).ToString();
+
+            Assert.AreEqual(selectStar, emptyWhere);
+            Assert.AreEqual(selectStar, nullWhere);
+        }
+
+        [Test]
+        public void Where_Clause_Gets_Valid_Predicate()
+        {
+            string predicate = "something > nothing";
+
+            string expected = String.Format("{0}&{1}={2}", selectStar, SoqlQuery.WhereKey, predicate);
+
+            string soql = new SoqlQuery().Where(predicate).ToString();
+
+            Assert.AreEqual(expected, soql);
+        }
+
+        [Test]
+        public void Last_Where_Overwrites_All_Previous()
+        {
+            string first = "first > 0";
+            string second = "second > first";
+            string last = "last > anything";
+
+            string expected = String.Format("{0}&{1}={2}", selectStar, SoqlQuery.WhereKey, last);
+
+            SoqlQuery soqlQuery = new SoqlQuery();
+
+            soqlQuery.Where(first)
+                     .Where(second)
+                     .Where(last);
+
+            Assert.AreEqual(expected, soqlQuery.ToString());
+        }
+
+        [Test]
+        public void Empty_Order_Ignores_Order_Clause()
+        {
+            string emptyGroup = new SoqlQuery().Order("").ToString();
+            string manyEmptyGroup = new SoqlQuery().Order("", "", "").ToString();
+            string nullGroup = new SoqlQuery().Order(null).ToString();
+
+            Assert.AreEqual(selectStar, emptyGroup);
+            Assert.AreEqual(selectStar, manyEmptyGroup);
+            Assert.AreEqual(selectStar, nullGroup);
+        }
+
+        [Test]
+        public void Default_Sort_Order_Is_Ascending()
+        {
+            string[] columns = { "column1", "column2" };
+
+            string expected = String.Format("{0}&{1}={2} {3}", selectStar, SoqlQuery.OrderKey, String.Join(SoqlQuery.Delimiter, columns), SortOrder.ASC);
+
+            string soql = new SoqlQuery().Order(columns).ToString();
+
+            Assert.AreEqual(expected, soql);
+        }
+
+        [TestCase(SortOrder.DESC, "column1", "")]
+        [TestCase(SortOrder.ASC, "column1", "", "column2")]
+        public void Order_Clause_Gets_Sort_Order_And_Only_Gets_Valid_Columns(SortOrder sortOrder, params string[] columns)
+        {
+            string expected = String.Format("{0}&{1}={2} {3}", selectStar, SoqlQuery.OrderKey, String.Join(SoqlQuery.Delimiter, columns.Where(c => !String.IsNullOrEmpty(c))), sortOrder);
+
+            string soql = new SoqlQuery().Order(sortOrder, columns).ToString();
+
+            Assert.AreEqual(expected, soql);
+        }
+
+        [Test]
+        public void Last_Order_Overwrites_All_Previous()
+        {
+            string[] first = { "first" };
+            string[] second = { "first", "second" };
+            string[] last = { "first", "second", "last" };
+            SortOrder sortOrder = SortOrder.ASC;
+
+            string expected = String.Format("{0}&{1}={2} {3}", selectStar, SoqlQuery.OrderKey, String.Join(SoqlQuery.Delimiter, last), sortOrder);
+
+            SoqlQuery soqlQuery = new SoqlQuery();
+
+            soqlQuery.Order(sortOrder, first)
+                     .Order(sortOrder, second)
+                     .Order(sortOrder, last);
+
+            Assert.AreEqual(expected, soqlQuery.ToString());
+        }
+
+        [Test]
+        public void Empty_Group_Ignores_Group_Clause()
+        {
+            string emptyGroup = new SoqlQuery().Group("").ToString();
+            string manyEmptyGroup = new SoqlQuery().Group("", "", "").ToString();
+            string nullGroup = new SoqlQuery().Group(null).ToString();
+
+            Assert.AreEqual(selectStar, emptyGroup);
+            Assert.AreEqual(selectStar, manyEmptyGroup);
+            Assert.AreEqual(selectStar, nullGroup);
+        }
+
+        [TestCase("column1", "")]
+        [TestCase("column1", "column2", "")]
+        public void Group_Clause_Only_Gets_Valid_Columns(params string[] columns)
+        {
+            string expected = String.Format("{0}&{1}={2}", selectStar, SoqlQuery.GroupKey, String.Join(SoqlQuery.Delimiter, columns.Where(c => !String.IsNullOrEmpty(c))));
+
+            string soql = new SoqlQuery().Group(columns).ToString();
+
+            Assert.AreEqual(expected, soql);
+        }
+
+        [Test]
+        public void Last_Group_Overwrites_All_Previous()
+        {
+            string[] first = { "first" };
+            string[] second = { "first", "second" };
+            string[] last = { "first", "second", "last" };
+
+            string expected = String.Format("{0}&{1}={2}", selectStar, SoqlQuery.GroupKey, String.Join(SoqlQuery.Delimiter, last));
+
+            SoqlQuery soqlQuery = new SoqlQuery();
+
+            soqlQuery.Group(first)
+                     .Group(second)
+                     .Group(last);
+
+            Assert.AreEqual(expected, soqlQuery.ToString());
+        }
+
+        [TestCase(-1)]
+        [TestCase(-999)]
+        [TestCase(1)]
+        [TestCase(999)]
+        public void Limit_Clause_Gets_Absolute_Limit(int limit)
+        {
+            string expected = String.Format("{0}&{1}={2}", selectStar, SoqlQuery.LimitKey, Math.Abs(limit));
+
+            string soql = new SoqlQuery().Limit(limit).ToString();
+
+            Assert.AreEqual(expected, soql);
+        }
+
+        [TestCase(1000)]
+        [TestCase(1001)]
+        [TestCase(9999)]
+        public void Limit_Max_Is_1000(int limit)
+        {
+            string expected = String.Format("{0}&{1}={2}", selectStar, SoqlQuery.LimitKey, 1000);
+
+            string soql = new SoqlQuery().Limit(limit).ToString();
+
+            Assert.AreEqual(expected, soql);
+        }
+
+        [Test]
+        public void Last_Limit_Overwrites_All_Previous()
+        {
+            int first = 1;
+            int second = 2;
+            int last = 3;
+
+            string expected = String.Format("{0}&{1}={2}", selectStar, SoqlQuery.LimitKey, last);
+
+            SoqlQuery soqlQuery = new SoqlQuery();
+
+            soqlQuery.Limit(first)
+                     .Limit(second)
+                     .Limit(last);
+
+            Assert.AreEqual(expected, soqlQuery.ToString());
+        }
+
+        [TestCase(-1)]
+        [TestCase(-999)]
+        [TestCase(1)]
+        [TestCase(999)]
+        public void Offset_Clause_Gets_Absolute_Offset(int offset)
+        {
+            string expected = String.Format("{0}&{1}={2}", selectStar, SoqlQuery.OffsetKey, Math.Abs(offset));
+
+            string soql = new SoqlQuery().Offset(offset).ToString();
+
+            Assert.AreEqual(expected, soql);
+        }
+
+        [Test]
+        public void Last_Offset_Overwrites_All_Previous()
+        {
+            int first = 1;
+            int second = 2;
+            int last = 3;
+
+            string expected = String.Format("{0}&{1}={2}", selectStar, SoqlQuery.OffsetKey, last);
+
+            SoqlQuery soqlQuery = new SoqlQuery();
+
+            soqlQuery.Offset(first)
+                     .Offset(second)
+                     .Offset(last);
+
+            Assert.AreEqual(expected, soqlQuery.ToString());
+        }
+    }
+}
