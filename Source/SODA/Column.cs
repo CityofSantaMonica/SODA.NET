473ea4f27c3b364cc82dc93ee1aa3f3cd6b3b4ec
Kegan Maher <Kegan.Maher@SMGOV.NET>
2014-07-16 18:50:07 +0000

    moving models up the namespace

---
+++ Source/SODA/Column.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace SODA.Models
+{
+    [DataContract]
+    public class Column
+    {
+        public string Id { get; set; }
+
+        public string DataTypeName { get; set; }
+
+        public string FieldName { get; set; }
+
+        public string Name { get; set; }
+
+        public int Position { get; set; }
+
+        public string RenderType { get; set; }
+
+        public string TableColumnId { get; set; }
+
+        public string Format { get; set; }
+
+        public string SubColumnTypes { get; set;}
+    }
+}
