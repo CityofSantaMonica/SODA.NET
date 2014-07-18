5f9216b80bdabbdc0f6010dc19b75b3ee790d1dc
Kegan Maher <Kegan.Maher@SMGOV.NET>
2014-07-18 15:28:15 +0000

    refactoring Uri helpers

--- Source/SODA/SodaUri.cs
+++ Source/SODA/SodaUri.cs
@@ -1,61 +1,84 @@
 ﻿using System;
+using SODA.Utilities;
 
 namespace SODA
 {
     public class SodaUri
     {
-        private static string metadataUrl(string domain, string resourceId)
+        private static string metadataUrl(string domain, string resourceId = null)
         {
-            return String.Format("https://{0}/views/{1}", domain, resourceId);
+            string url = String.Format("https://{0}/views", domain);
+
+            if(resourceId.HasValue())
+            {
+                url = String.Format("{0}/{1}", url, resourceId);
+            }
+
+            return url;
         }
 
-        public static Uri ForMetadata(string domain, string datasetId)
+        public static Uri ForMetadata(string domain, string resourceId)
         {
-            string url = metadataUrl(domain, datasetId);
+            string url = metadataUrl(domain, resourceId);
 
             return new Uri(url);
         }
 
-        public static Uri ForResource(string domain, string resourceId, SodaResponseFormat format = SodaResponseFormat.JSON, string rowId = null)
+        public static Uri ForMetadataList(string domain, int page)
+        {
+            if (page > 0)
+            {
+                string url = String.Format("{0}?page={1}", metadataUrl(domain), page);
+
+                return new Uri(url);
+            }
+
+            return default(Uri);
+        }
+
+        public static Uri ForResource(string domain, string resourceId, string rowId = null)
         {
             string url = metadataUrl(domain, resourceId).Replace("views", "resource");
 
-            if(String.IsNullOrEmpty(rowId))
-                url = String.Format("{0}.ext", url);
-            else
-                url = String.Format("{0}/{1}.ext", url, rowId);
-
-            if (format == SodaResponseFormat.XML)
+            if (rowId.HasValue())
             {
-                url = url.Replace(".ext", ".xml");
-            }
-            else
-            {
-                url = url.Replace(".ext", ".json");
+                url = String.Format("{0}/{1}", url, rowId);
             }
 
             return new Uri(url);
         }
 
-        public static Uri ForCatalog(string domain, int page)
+        public static Uri ForResourcePermalink(string domain, string resourceId)
         {
-            string url = String.Format("https://{0}/api/views?page={1}", domain, page);
+            string url = metadataUrl(domain, resourceId).Replace("views", "-/-");
 
             return new Uri(url);
         }
 
-        public static Uri ForQuery(string domain, string datasetId, SoqlQuery soqlQuery)
+        public static Uri ForQuery(string domain, string resourceId, SoqlQuery soqlQuery)
         {
-            return ForQuery(domain, datasetId, soqlQuery.ToString());
+            return ForQuery(domain, resourceId, soqlQuery.ToString());
         }
 
-        public static Uri ForQuery(string domain, string datasetId, string soqlQuery)
+        public static Uri ForQuery(string domain, string resourceId, string soqlQuery)
         {
-            string url = metadataUrl(domain, datasetId);
+            string url = metadataUrl(domain, resourceId);
 
             string queryUrl = String.Format("{0}?{1}", url, soqlQuery);
 
             return new Uri(queryUrl);
+        }
+        
+        public static Uri ForCategoryPage(string domain, string category)
+        {
+            if (category.HasValue())
+            {
+                string url = String.Format("{0}/{1}", metadataUrl(domain).Replace("views", "categories"), Uri.EscapeDataString(category));
+
+                return new Uri(url);
+            }
+
+            return default(Uri);
         }
     }
 }
