df66cac997868b1f638d8191c17bb337674960e4
Kegan Maher <Kegan.Maher@SMGOV.NET>
2014-07-29 21:50:04 +0000

    moving DateTimeConverter from SODA.Utilities assembly to SODA assembly

---
+++ Source/SODA.Tests/Unit/DateTimeConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using SODA.Utilities;
+
+namespace SODA.Tests.Unit
+{
+    [TestFixture]
+    public class DateTimeConverterTests
+    {
+        [Test]
+        [Category("DateTimeConverter")]
+        public void FromUnix_Converts_Unix_Timestamp_To_Local_Time()
+        {
+            DateTime expected = DateTime.Now;
+
+            double unixTimeStamp = (expected.ToUniversalTime().Subtract(DateTimeConverter.UnixEpoch)).TotalSeconds;
+
+            DateTime actual = DateTimeConverter.FromUnixTimestamp(unixTimeStamp);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
