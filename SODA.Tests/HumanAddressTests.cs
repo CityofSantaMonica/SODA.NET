81851ae70dde64b51d2d8c16c017f7034ac6d059
Kegan Maher <Kegan.Maher@SMGOV.NET>
2014-08-05 00:36:00 +0000

    define a HumanAddress object for location columns

---
+++ SODA.Tests/HumanAddressTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using NUnit.Framework;
+using SODA.Models;
+
+namespace SODA.Tests
+{
+    [TestFixture]
+    public class HumanAddressTests
+    {
+        string address = "1234 Test Street";
+        string city = "TestVille";
+        string state = "CA";
+        string zip = "67890";
+        string json;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            json = String.Format(@"{{""address"":""{0}"",""city"":""{1}"",""state"":""{2}"",""zip"":""{3}""}}", address, city, state, zip);
+        }
+
+        [Test]
+        [Category("HumanAddress")]
+        public void New_Deserializes_Valid_HumanAddress_Json()
+        {
+            HumanAddress humanAddress = new HumanAddress(json);
+
+            Assert.AreEqual(address, humanAddress.Address);
+            Assert.AreEqual(city, humanAddress.City);
+            Assert.AreEqual(state, humanAddress.State);
+            Assert.AreEqual(zip, humanAddress.Zip);
+        }
+
+        [TestCase("not json")]
+        [TestCase(@"{""not"":""a"",""human"":""address""}")]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [Category("HumanAddress")]
+        public void New_Throws_ArgumentOutOfRangeException_For_Invalid_HumanAddress_Json(string input)
+        {
+            new HumanAddress(input);
+        }
+
+        [Test]
+        [Category("HumanAddress")]
+        public void ToString_Serializes_As_Json()
+        {
+            HumanAddress humanAddress = new HumanAddress() {
+                Address = address,
+                City = city,
+                State = state,
+                Zip = zip,
+            };
+
+            string humanAddressToString = humanAddress.ToString();
+
+            Assert.AreEqual(json, humanAddressToString);
+        }
+
+        [Test]
+        [Category("HumanAddress")]
+        public void Implicit_String_Conversion_Returns_Serialized_Json()
+        {
+            HumanAddress humanAddress = new HumanAddress() {
+                Address = address,
+                City = city,
+                State = state,
+                Zip = zip,
+            };
+
+            string humanAddressAsString = humanAddress;
+
+            Assert.AreEqual(json, humanAddressAsString);
+        }
+
+        [Test]
+        [Category("HumanAddress")]
+        public void Explicit_HumanAddress_Conversion_Returns_HumanAddress_Object()
+        {
+            HumanAddress expected = new HumanAddress() {
+                Address = address,
+                City = city,
+                State = state,
+                Zip = zip
+            };
+
+            HumanAddress actual = (HumanAddress)json;
+
+            Assert.AreEqual(expected.Address, actual.Address);
+            Assert.AreEqual(expected.City, actual.City);
+            Assert.AreEqual(expected.State, actual.State);
+            Assert.AreEqual(expected.Zip, actual.Zip);
+        }
+    }
+}
--- SODA/Models/LocationColumn.cs
+++ SODA/Models/LocationColumn.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace SODA.Models
 {
@@ -16,5 +18,81 @@ namespace SODA.Models
 
         [DataMember(Name = "human_address")]
         public string HumanAddress { get; set; }
+    }
+
+    [DataContract]
+    public class HumanAddress
+    {
+        [DataMember(Name="address")]
+        public string Address { get; set; }
+
+        [DataMember(Name = "city")]
+        public string City { get; set; }
+
+        [DataMember(Name = "state")]
+        public string State { get; set;}
+
+        [DataMember(Name = "zip")]
+        public string Zip { get; set; }
+
+        public HumanAddress()
+        {
+        }
+
+        /// <summary>
+        /// Construct a new HumanAddress object from its JSON string representation.
+        /// </summary>
+        /// <param name="humanAddressJson">The serialized JSON string of a HumanAddress object.</param>
+        public HumanAddress(string humanAddressJson)
+        {
+            HumanAddress other = null;
+
+            try
+            {
+                other = JsonConvert.DeserializeObject<HumanAddress>(humanAddressJson);
+            }
+            catch
+            {
+                throw new ArgumentOutOfRangeException("humanAddressJson", "The provided data was not parsable to a HumanAddress object.");
+            }
+            if (other != null && other.Address == null && other.City == null && other.State == null && other.Zip == null)
+            {
+                throw new ArgumentOutOfRangeException("humanAddressJson", "The provided data was not parsable to a HumanAddress object.");
+            }
+
+            Address = other.Address;
+            City = other.City;
+            State = other.State;
+            Zip = other.Zip;
+        }
+
+        /// <summary>
+        /// Converts this HumanAddress object to its JSON-string representation.
+        /// </summary>
+        /// <returns>The serialized JSON string of this HumanAddress</returns>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        /// <summary>
+        /// Converts a HumanAddress object to its JSON string representation.
+        /// </summary>
+        /// <param name="humanAddress">A HumanAddress object to convert.</param>
+        /// <returns>The serialized JSON string of the specified HumanAddress</returns>
+        public static implicit operator string(HumanAddress humanAddress)
+        {
+            return humanAddress.ToString();
+        }
+
+        /// <summary>
+        /// Converts a JSON string representation of a HumanAddress object into a populated HumanAddress object.
+        /// </summary>
+        /// <param name="humanAddressJson">The serialized JSON string of a HumanAddress object.</param>
+        /// <returns>A HumanAddress object with data deserialized out of the specified JSON string.</returns>
+        public static explicit operator HumanAddress(string humanAddressJson)
+        {
+            return new HumanAddress(humanAddressJson);
+        }
     }
 }
