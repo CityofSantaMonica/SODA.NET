6d80fab54b63fde70235f0b76a8e13af7870ac90
Kegan Maher <Kegan.Maher@SMGOV.NET>
2014-08-07 18:17:48 +0000

    use the WebsiteUrlColumn

---
+++ SODA.Tests/WebsiteUrlColumnTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using SODA.Utilities;
+using SODA.Models;
+using SODA.Tests.Mocks;
+
+namespace SODA.Tests
+{
+    [TestFixture]
+    public class WebsiteUrlColumnTests
+    {
+        [TestCase(StringMocks.EmptyInput)]
+        [TestCase(StringMocks.NullInput)]
+        [Category("WebsiteUrlColumn")]
+        public void New_With_Empty_Url_Gets_Empty_Url(string input)
+        {
+            var column = new WebsiteUrlColumn(input, StringMocks.NonEmptyInput);
+
+            Assert.IsEmpty(column.Url);
+        }
+
+        [TestCase("http://www.example.com/Spaces In The Url")]
+        [TestCase("http://www.example.com/Special:Chars,In(The > Url)")]
+        [Category("WebsiteUrlColumn")]
+        public void New_With_Url_Gets_Uri_Escaped_Url(string input)
+        {
+            var column = new WebsiteUrlColumn(input, StringMocks.NonEmptyInput);
+
+            string expected = Uri.EscapeUriString(input);
+
+            Assert.AreEqual(expected, column.Url);
+        }
+
+        [Test]
+        [Category("WebsiteUrlColumn")]
+        public void New_With_Uri_Gets_Url_From_Uri()
+        {
+            var uri = new Uri("http://www.example.com");
+
+            var column = new WebsiteUrlColumn(uri, StringMocks.NonEmptyInput);
+
+            Assert.AreEqual(uri.ToString(), column.Url);
+        }
+
+        [Test]
+        [Category("WebsiteUrlColumn")]
+        public void Serializes_To_Socrata_Expected_JSON()
+        {
+            string url = "http://www.example.com";
+            string urlJson = String.Format(@"""url"":""{0}""", url);
+            string description = "the description";
+            string descriptionJson = String.Format(@"""description"":""{0}""", description);
+            
+            var column = new WebsiteUrlColumn(url, description);
+
+            var columnJson = column.ToJsonString();
+
+            StringAssert.Contains(urlJson, columnJson);
+            StringAssert.Contains(descriptionJson, columnJson);
+        }
+    }
+}
--- SODA/Models/WebsiteUrlColumn.cs
+++ SODA/Models/WebsiteUrlColumn.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 
 namespace SODA.Models
 {
@@ -10,5 +11,15 @@ namespace SODA.Models
 
         [DataMember(Name="url")]
         public string Url { get; set; }
+
+        public WebsiteUrlColumn() { }
+
+        public WebsiteUrlColumn(string url, string description)
+        {
+            Url = String.IsNullOrEmpty(url) ? String.Empty : Uri.EscapeUriString(url);
+            Description = description;
+        }
+
+        public WebsiteUrlColumn(Uri uri, string description) : this(uri.ToString(), description) { }
     }
 }
