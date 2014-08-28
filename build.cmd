dce6aece170f15730ceceb76ecc2b81b00524f13
Kegan Maher <Kegan.Maher@SMGOV.NET>
2014-08-28 18:07:04 +0000

    making the build script more flexible

--- build.cmd
+++ build.cmd
@@ -1,17 +1,39 @@
 @echo off
 
-:: make sure we have nuget.exe in the path
-set PATH=%PATH%;%~dp0tools\nuget
+cls
 
-:: make sure nuget is updated
-nuget update -self
+set TARGET="Default"
+if not "%1" == "" (set TARGET="%1")
+echo %TARGET%
 
-:: build the CSM.SodaDotNet nuget package
-cd SODA
-nuget pack SODA.csproj -Prop Configuration=Release
+set BUILDMODE="Release"
+if not "%2" == "" (set BUILDMODE="%2")
 
-:: build the CSM.SodaDotNet.Utilities nuget package
-cd ..\Soda.Utilities
-nuget pack SODA.Utilities.csproj -IncludeReferencedProjects -Prop Configuration=Release
+set MSBUILDDIR="%WINDIR%\Microsoft.NET\Framework\v4.0.30319"
+set NUGETDIR=".\tools\nuget"
+set SODADIR=".\SODA"
+set UTILSDIR=".\SODA.Utilities"
+
+echo Rebuilding solution with Configuration: %BUILDMODE%
+
+call %MSBUILDDIR%\msbuild.exe SODA.sln /m "/p:Configuration=%BUILDMODE%" "/p:Platform=Any CPU" /t:Clean,Build	
+
+echo Finished solution rebuild
+
+if %TARGET% == "CreatePackages" (
+	echo Creating NuGet packages with Configuration: %BUILDMODE%
+
+	call xcopy %SODADIR%\bin\%BUILDMODE%\*.* %SODADIR%\lib\ /y
+	call xcopy %UTILSDIR%\bin\%BUILDMODE%\*.* %UTILSDIR%\lib\ /y
+
+	call %NUGETDIR%\nuget.exe pack %SODADIR%\SODA.csproj -Properties "Configuration=%BUILDMODE%;Platform=AnyCPU"
+	call %NUGETDIR%\nuget.exe pack %UTILSDIR%\SODA.Utilities.csproj -IncludeReferencedProjects -Properties "Configuration=%BUILDMODE%;Platform=AnyCPU"
+	
+	rd /S /Q %SODADIR%\lib
+	rd /S /Q %UTILSDIR%\lib
+
+	echo Finished NuGet package creation
+)
 
 pause
+exit /B 0
