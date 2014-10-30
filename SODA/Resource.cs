34289c1d9f38d604e9d82f7e3abac0e88e0961de
Kegan Maher <kegan.maher@smgov.net>
2014-10-30 08:06:43 -0700

    adding doc

--- SODA/Resource.cs
+++ SODA/Resource.cs
@@ -129,7 +129,7 @@ namespace SODA
         /// <returns>A collection of entities of type TRow.</returns>
         /// <remarks>
         /// This is a convenience method for the generic <see cref="Query{T}"/>, and is useful if you want the result of a query 
-        /// to be typed to this Resource's underlying record type.
+        /// to be typed to <typeparamref name="TRow"/>this Resource's underlying record type</typeparamref>.
         /// </remarks>
         public IEnumerable<TRow> Query(SoqlQuery soqlQuery)
         {
