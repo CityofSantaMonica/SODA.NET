10b6036097a5883023b31178f40222458f6a2fbf
Sevil Turner <sevil.turner@smgov.net>
2016-07-12 08:50:39 -0700

    Initial commit after removing all the classes

---
+++ SODA/Models/Coordinates.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using SODA.Utilities;
+
+namespace SODA.Models
+{
+    /// <summary>
+    /// Describes the Geographic Position - Longtitude and Latitude.
+    /// </summary>
+    [DataContract]
+    public class Coordinates
+    {
+        /// <summary>
+        /// Describes the Geographic Position For Longitude.
+        /// </summary>
+        [DataMember]
+        public readonly double x;
+
+        /// <summary>
+        /// Describes the Geographic Position for Latitude.
+        /// </summary>
+        [DataMember]
+        public readonly double y;       
+
+        /// <summary>
+        /// Constructor. Initializes a new instance of the Coordinates class.
+        /// </summary>        
+        public Coordinates(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+    }
+}
+
