92dc6892910c2aff2c12f61ef6c5fb1ce1c5ad6d
kevinasdzine <hobsonzchoice@gmail.com>
2016-07-21 08:34:33 -0700

    trying enum type

---
+++ SODA/Models/Geometry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SODA.Models
+{
+    class Geometry
+    {
+        public enum type
+        {
+            Point,
+            MultiPoint,
+            LineString,
+            MultiLineString,
+            Polygon,
+            MultiPolygon
+        }
+    }
+}
---
+++ SODA/Models/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SODA.Models
+{
+    [DataContract]
+    class Point:Geometry
+    {
+        [DataMember]
+        public new type type { get; set; }
+        [DataMember]
+        public double[] coordinates { get; set; }
+    }
+}
