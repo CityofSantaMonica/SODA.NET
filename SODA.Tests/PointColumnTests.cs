d9396e999be918ea213bbe6503bc31db0b90ad5b
Sevil Turner <sevil.turner@smgov.net>
2016-08-01 16:57:17 -0700

    Added a new class and a unit test for PointColumn

---
+++ SODA.Tests/PointColumnTests.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SODA.Models;
+
+namespace SODA.Tests
+{
+    [TestFixture]
+    [Category("Positions")]
+    public class PointColumnTests
+    {
+        [Test]
+        public void Can_Serialize_Point_Feature()
+        {
+            var point = new PointColumn(new Positions(new[] {-87.653274,41.936172}));
+
+            string expectedJSON = "{\"type\":\"Point\",\"coordinates\":[-87.653274,41.936172]}";
+
+            string actualJSON = JsonConvert.SerializeObject(point);
+
+            Assert.AreEqual(actualJSON, expectedJSON);
+            Assert.IsNotNull(actualJSON);
+        }
+
+
+        [Test]
+        public void Can_Deserialize_Point_Feature()
+        {
+            var json = "{\"type\":\"Point\",\"coordinates\":[-87.653274,41.936172]}";
+
+            var actualPointColumn = JsonConvert.DeserializeObject<PointColumn>(json);
+
+            Assert.IsNotNull(actualPointColumn);
+            Assert.AreEqual(actualPointColumn.Coordinates.PositionsArray[0], -87.653274);
+            Assert.AreEqual(actualPointColumn.Coordinates.PositionsArray[1], 41.936172);                
+        }        
+    }
+}
+
---
+++ SODA/Models/PointColumn.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Serialization;
+
+namespace SODA.Models
+{
+    /// <summary>
+    /// A class to model the Socrata Point column type that consist of a single position.
+    /// </summary>
+    [DataContract]
+    public class PointColumn
+    {
+        private PointColumn()
+        {
+            Type = "Point";
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PointColumn"/> class.
+        /// </summary>
+        /// <param name="coordinates">The coordinates.</param>
+        public PointColumn(Positions coordinates) : this()
+        {
+            Coordinates = coordinates;
+        }
+
+        /// <summary>
+        /// Gets or sets the string representation of the type value for this PointColumn.
+        /// </summary>
+        [DataMember(Name = "type")]
+        public string Type { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the string representation of the coordinates value for this PointColumn.
+        /// </summary>
+        [DataMember(Name = "coordinates")]
+        public Positions Coordinates { get; private set; }
+    }
+}
