ba1b09c1843a6b635e8dec4d82cd2341ff44cf55
Sevil Turner <sevil.turner@smgov.net>
2016-09-19 16:04:40 -0700

    Added MultiLine datatype.

---
+++ SODA.Tests/MultiLineColumnTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SODA.Models;
+using System.Collections.Generic;
+
+namespace SODA.Tests
+{
+    [TestFixture]
+    [Category("MultiLineColumn")]
+    public class MultiLineColumnTests
+    {
+        List<List<Positions>> positions;
+
+        [SetUp]
+        public void SetUp()
+        {
+            positions = new List<List<Positions>>
+            {
+                new List<Positions>(){new Positions(new[] { 100.0, 0.0 }),{new Positions(new[] { 101.0, 1.0 })}},
+                new List<Positions>(){new Positions(new[] { 102.0, 2.0 }),{new Positions(new[] { 103.0, 3.0 })}}
+            };
+        }
+
+        [Test]
+        public void Can_Serialize_MultiLineString_Feature()
+        {
+            var expected =
+                String.Format(
+                    "{{\"type\":\"MultiLineString\",\"coordinates\":[[[{0:F1},{1:F1}],[{2:F1},{3:F1}]],[[{4:F1},{5:F1}],[{6:F1},{7:F1}]]]}}",
+                    positions[0][0].PositionsArray[0],
+                    positions[0][0].PositionsArray[1],
+                    positions[0][1].PositionsArray[0],
+                    positions[0][1].PositionsArray[1],
+                    positions[1][0].PositionsArray[0],
+                    positions[1][0].PositionsArray[1],
+                    positions[1][1].PositionsArray[0],
+                    positions[1][1].PositionsArray[1]);
+
+            var column = new MultiLineColumn(positions);
+
+            var actualJson = JsonConvert.SerializeObject(column);
+
+            Assert.AreEqual(expected, actualJson);
+        }
+
+        [Test]
+        public void Can_Deserialize_MultiLineString_Feature()
+        {
+            var jsonResult = String.Format(
+                    "{{\"type\":\"MultiLineString\",\"coordinates\":[[[{0:F1},{1:F1}],[{2:F1},{3:F1}]],[[{4:F1},{5:F1}],[{6:F1},{7:F1}]]]}}",
+                    positions[0][0].PositionsArray[0],
+                    positions[0][0].PositionsArray[1],
+                    positions[0][1].PositionsArray[0],
+                    positions[0][1].PositionsArray[1],
+                    positions[1][0].PositionsArray[0],
+                    positions[1][0].PositionsArray[1],
+                    positions[1][1].PositionsArray[0],
+                    positions[1][1].PositionsArray[1]);
+
+            var multiLineColumn = JsonConvert.DeserializeObject<MultiLineColumn>(jsonResult);
+
+            Assert.AreEqual("MultiLineString", multiLineColumn.Type);
+
+            Assert.AreEqual(100.0, multiLineColumn.Coordinates[0][0].PositionsArray[0]);
+            Assert.AreEqual(0.0, multiLineColumn.Coordinates[0][0].PositionsArray[1]);
+
+            Assert.AreEqual(101.0, multiLineColumn.Coordinates[0][1].PositionsArray[0]);
+            Assert.AreEqual(1.0, multiLineColumn.Coordinates[0][1].PositionsArray[1]);
+
+            Assert.AreEqual(102.0, multiLineColumn.Coordinates[1][0].PositionsArray[0]);
+            Assert.AreEqual(2.0, multiLineColumn.Coordinates[1][0].PositionsArray[1]);
+
+            Assert.AreEqual(103.0, multiLineColumn.Coordinates[1][1].PositionsArray[0]);
+            Assert.AreEqual(3.0, multiLineColumn.Coordinates[1][1].PositionsArray[1]);
+        }
+    }
+}
---
+++ SODA/Models/MultiLineColumn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace SODA.Models
+{
+    /// <summary>
+    /// A class to model the Socrata MultiLine column type that consist of an array of LineString coordinate arrays.
+    /// </summary>
+    [DataContract]
+    public class MultiLineColumn
+    {
+        private MultiLineColumn()
+        {
+            Type = "MultiLineString";
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiLineColumn"/> class.
+        /// </summary>
+        /// <param name="coordinates">The coordinates.</param>
+        public MultiLineColumn(List<List<Positions>> coordinates) : this()
+        {
+            Coordinates = coordinates;
+        }
+
+        /// <summary>
+        /// Gets the type value for this MultiLineColumn.
+        /// </summary>
+        [DataMember(Name = "type")]
+        public string Type { get; private set; }
+
+        /// <summary>
+        /// Gets the <see cref="Positions"/> for this MultiLineColumn.
+        /// </summary>
+        [DataMember(Name = "coordinates")]
+        public List<List<Positions>> Coordinates { get; private set; }
+    }
+}
