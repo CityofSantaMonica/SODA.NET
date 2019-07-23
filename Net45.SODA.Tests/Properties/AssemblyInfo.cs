ab4992ef73dee903a9289f30f889936a71b2e012
Kegan Maher <kegan.maher@smgov.net>
2019-07-23 11:39:56 -0700

    solution reorg

---
+++ Net45.SODA.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Net45.SODA.Tests")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("309fa03e-e032-4547-a83c-389ec450fd1a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Net45.Utilities.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Net45.Utilities.Tests")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("763fffcb-49dd-4a86-b5a1-b51ea15ce764")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
